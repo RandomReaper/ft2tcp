9708767942fc99da03e60b86eb37f3bb86700f39
Marc Pignat <marc@pignat.org>
2016-03-03 14:43:57 +0100

    Added win32 target for cross compiling, tested and working

--- pim-compile.sh
+++ pim-compile.sh
@@ -4,14 +4,23 @@ MXE=~/git/mxe/
 
 mkdir -p build/native
 mkdir -p build/win64
+mkdir -p build/win32
 cd build/native
-qmake ../..
+qmake ../../ft2tcp.pro
 make -j9
 cd ../..
 
 QMAKE=$MXE/usr/x86_64-w64-mingw32.static/qt5/bin/qmake
 PATH=$MXE/usr/bin:$PATH
 cd build/win64
-$QMAKE ../..
+$QMAKE ../../ft2tcp.pro
 make -j9
 cd ../..
+
+
+QMAKE=$MXE/usr/i686-w64-mingw32.static/qt5/bin/qmake
+cd build/win32
+$QMAKE ../../ft2tcp.pro
+make -j9
+cd ../..
+
